fdf78263dd5025dcfe673ee708d795632a7cadae
libin <civet148@126.com>
2023-08-10 10:11:26 +0800

    sort of bubble and insert

--- Sort/quick/main.go
+++ Sort/quick/main.go
@@ -4,7 +4,9 @@ import "fmt"
 
 func main() {
 	elems := []int{32, 12, 34, 42, 88, 312, 12, 1, 0, 99}
+	fmt.Printf("快速排序：开始 %v \n", elems)
 	Sort(elems)
+	fmt.Printf("快速排序：结束 %v \n", elems)
 }
 
 /*
@@ -16,7 +18,23 @@ func main() {
 */
 
 func Sort(elems []int) {
-	fmt.Printf("快速排序：开始 %v \n", elems)
 
-	fmt.Printf("快速排序：结束 %v \n", elems)
+	if len(elems) < 2 {
+		return
+	}
+	value := elems[0]
+	head, trip := 0, len(elems)-1
+	for head < trip {
+		if elems[head+1] > value { //当前元素大于标尺元素换到右边(trip--)
+			elems[head+1], elems[trip] = elems[trip], elems[head+1]
+			trip--
+		} else if elems[head+1] < value { //当前元素小于标尺元素换到左边(head++)
+			elems[head], elems[head+1] = elems[head+1], elems[head]
+			head++
+		} else { //当前元素等于标尺元素(head++)
+			head++
+		}
+	}
+	Sort(elems[:head])
+	Sort(elems[head+1:])
 }
