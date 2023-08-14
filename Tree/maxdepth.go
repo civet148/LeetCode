df0f714996b3737a64d0aff3104960d6567bc8ac
libin <civet148@126.com>
2023-08-14 08:08:51 +0800

    tree max depth example

--- Tree/maxdepth.go
+++ Tree/maxdepth.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
@@ -7,7 +9,19 @@ type TreeNode struct {
 }
 
 func main() {
-
+	var root = &TreeNode{
+		Val: 1,
+		Left: &TreeNode{
+			Val: 2,
+			Left: &TreeNode{
+				Val: 3,
+			},
+		},
+		Right: &TreeNode{
+			Val: 3,
+		},
+	}
+	fmt.Printf("tree depth %v\n", depth(root))
 }
 
 func depth(root *TreeNode) int {
